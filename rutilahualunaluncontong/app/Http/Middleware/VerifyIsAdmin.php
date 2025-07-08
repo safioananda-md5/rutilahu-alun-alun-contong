a95f9e5e1659f906ab53ec8afb6ca0cb99e56f9a
KHARISMA SAFIO ANANDA <safiopertama@gmail.com>
2025-07-08 09:54:57 +0700

    Membuat middleware isAdmin

---
+++ rutilahualunaluncontong/app/Http/Middleware/VerifyIsAdmin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifyIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $role_id = $request->user()->role_id;
+        dd($role_id);
+        return $next($request);
+    }
+}
