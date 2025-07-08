d4deceb9e2a91e560f2d96d7620c849c57d7f61a
KHARISMA SAFIO ANANDA <safiopertama@gmail.com>
2025-07-08 09:38:36 +0700

    membuat tabel role, users, dan koneksi role_id

--- rutilahualunaluncontong/database/migrations/0001_01_01_000000_create_users_table.php
+++ rutilahualunaluncontong/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nik');
+            $table->string('no_kk');
+            $table->string('address');
+            $table->string('phone_number');
             $table->rememberToken();
             $table->timestamps();
         });
--- rutilahualunaluncontong/database/seeders/RoleSeeder.php
+++ rutilahualunaluncontong/database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $roles = ['admin', 'rt', 'rw', 'warga'];
+
+        foreach ($roles as $role){
+            Role::create([
+                'role_name' => $role
+            ]);
+        }
     }
 }
